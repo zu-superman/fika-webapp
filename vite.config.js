dd7ade97773010357c8b29dcb3d16d4e2d90df0c
zu-superman <superman@zusf.com>
2026-05-08 09:38:26 +0800

    ``` feat(vite): 添加环境变量加载和服务器端口配置
    - 引入 loadEnv 函数以支持环境变量加载
    - 配置服务器端口从环境变量 VITE_PORT 读取，默认值为 8000
    - 修改 defineConfig 以接收模式参数并返回配置对象
    ```

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -6,19 +6,26 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+export default defineConfig(({ mode }) =>{
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    server: {
+      port: env.VITE_PORT || 8000,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }
 })
